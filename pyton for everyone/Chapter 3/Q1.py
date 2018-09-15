e92b0c99060d15fa072db52b60d270863b110edd
Aized Sumbal <aizedsumbal@gmail.com>
2018-09-15 13:54:49 +0500

    DEDE

---
+++ pyton for everyone/Chapter 3/Q1.py
@@ -0,0 +1,8 @@
+check=input("Enter any integer:")
+check=int(check)
+if (check<0):
+    print("Your integer is negative.")
+elif (check>0):
+    print("Your integer is positive.")
+elif(check==0):
+    print("Your integer is nuetral.")
