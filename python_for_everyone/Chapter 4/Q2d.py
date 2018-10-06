655eb85cc34d05c5dd6b2e5ef7056d27d8f76783
Aized Sumbal <aizedsumbal@gmail.com>
2018-10-07 00:35:45 +0500

    python for everyone

---
+++ python_for_everyone/Chapter 4/Q2d.py
@@ -0,0 +1,7 @@
+unique = 0
+unique_str = ""
+for i in range():
+    nums = int(input("Enter your number"))
+
+    if unique == nums:
+        unique = unique + unique_str
---
+++ python_for_everyone/Chapter 5/Q1a.py
@@ -0,0 +1,12 @@
+num1 = input("Enter your number")
+num2 = input("Enter your number")
+num3 = input("Enter your number")
+
+
+def smallest(x, y, z):
+    return min(x, y, z)
+
+
+a = smallest(num1, num2, num3)
+
+print(a)
