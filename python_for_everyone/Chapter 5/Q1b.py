c45701d6884ffd7fdb1baf06bb9477daedf58f6e
Aized Sumbal <aizedsumbal@gmail.com>
2018-10-07 10:43:57 +0500

    python for everyone

---
+++ python_for_everyone/Chapter 5/Q1b.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter your number"))
+num2 = int(input("Enter your number"))
+num3 = int(input("Enter your number"))
+
+
+def average(num1 , num2 , num3):
+    return (num1 + num2 + num3) / 3
+
+
+a = average(num1, num2, num3)
+print(a)
+
+
